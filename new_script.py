4257ee87cf5dd6a3de7b28b82c1ec97b1ce127a4
Senuri Wijenayake <Senuri Wijenayake>
2017-05-30 18:13:17 +0530

    Janitha testing server

--- new_script.py
+++ new_script.py
@@ -1,5 +1,9 @@
 from eval import *
 from similarities import *
 
-calculate_errors()
+#calculate_errors()
+user = "1665852693730402"
+locations = ["ChIJ5VeV5R434joRUXuKDoS6hos","ChIJ7-MWGe5a4joRGU1D7xL_UNs"]
+x = rateLocations(training_data,user,locations,avgs,all_sims)
 
+print (x)
