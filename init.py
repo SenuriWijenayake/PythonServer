1166ffe08b1321fd37407b6ce0cf215fdb0bf802
Senuri Wijenayake <Senuri Wijenayake>
2017-06-01 11:32:50 +0530

    Created one function for ratings

--- init.py
+++ init.py
@@ -6,68 +6,69 @@ import warnings
 import time
 
 warnings.simplefilter("ignore", category=DeprecationWarning)
-
 start_time = time.time()
 
-#Define the similarity measurement
-similarity = pearson_network_similarity_basic
+def plan_my_trip(user,locations):
 
-#Initializing the test and training data sets for use
-#Training set includes only 40 users and half of their preferences
-training_data,test_data,new_users = initializeDataSet()
+    #Define the similarity measurement
+    similarity = pearson_network_similarity_basic
 
-#Getting the user average rating values
-print ("Calculating user average ratings")
-avgs = calAverages(training_data)
+    #Initializing the test and training data sets for use
+    #Training set includes only 40 users and half of their preferences
+    training_data,test_data,new_users = initializeDataSet()
 
-#Initializing the regression model
-print ("Initilizing the regression model")
-data = pd.read_csv('csv/TrainingSet/final_training_set.csv', sep=',', na_values="")
+    #Getting the user average rating values
+    print ("Calculating user average ratings")
+    avgs = calAverages(training_data)
 
-# Train the model
-X = data[['gender','locations_together', 'mutual_strength', 'likes']]
-y = data.response
-lm = LinearRegression(normalize=False)
-lm.fit(X, y)
+    #Initializing the regression model
+    print ("Initilizing the regression model")
+    data = pd.read_csv('csv/TrainingSet/final_training_set.csv', sep=',', na_values="")
 
-#Calcualting the user-user similiarities based on the defined similarity measure
-print ("Calculating all user similarities")
-all_sims = calSimilarities(training_data,avgs,similarity,lm)
+    # Train the model
+    X = data[['gender','locations_together', 'mutual_strength', 'likes']]
+    y = data.response
+    lm = LinearRegression(normalize=False)
+    lm.fit(X, y)
 
-print ("Initialization completed")
-#print (all_sims)
+    #Calcualting the user-user similiarities based on the defined similarity measure
+    print ("Calculating all user similarities")
+    all_sims = calSimilarities(training_data,avgs,similarity,lm)
 
-user = "1665852693730402"
-#user = "16658526937304023"
-locations = [
-    {
-        "area" : " Peradeniya",
-         "latitude" : 7.2565929,
-         "name" : "University of Peradeniya",
-         "id" : "ChIJ89kP1Lkv3YARr3RXkCQuMsY",
-         "longitude" : 80.5966093,
-         "rating" : 4.7,
-         "types" : [
-             "university",
-            "point_of_interest",
-            "establishment"
-    ]},
-    {
-        "name" : "National Museum of Galle",
-        "longitude" : 80.2170162,
-        "latitude" : 6.0288631,
-        "rating" : 4.2,
-        "types" : [
-            "museum",
-            "point_of_interest",
-            "establishment"
-        ],
-        "id" : "ChIJs_O3o0BZ4joRmOpvo3RHzms",
-        "area" : " Galle"
-    }
-]
+    print ("Initialization completed")
+    #print (all_sims)
 
-x = rateLocations(training_data,user,locations,avgs,all_sims)
+    # user = "1665852693730402"
+    # #user = "16658526937304023"
+    # locations = [
+    #     {
+    #         "area" : " Peradeniya",
+    #          "latitude" : 7.2565929,
+    #          "name" : "University of Peradeniya",
+    #          "id" : "ChIJ89kP1Lkv3YARr3RXkCQuMsY",
+    #          "longitude" : 80.5966093,
+    #          "rating" : 4.7,
+    #          "types" : [
+    #              "university",
+    #             "point_of_interest",
+    #             "establishment"
+    #     ]},
+    #     {
+    #         "name" : "National Museum of Galle",
+    #         "longitude" : 80.2170162,
+    #         "latitude" : 6.0288631,
+    #         "rating" : 4.2,
+    #         "types" : [
+    #             "museum",
+    #             "point_of_interest",
+    #             "establishment"
+    #         ],
+    #         "id" : "ChIJs_O3o0BZ4joRmOpvo3RHzms",
+    #         "area" : " Galle"
+    #     }
+    # ]
 
-print (x)
-print("--- %s seconds ---" % (time.time() - start_time))
+    x = rateLocations(training_data,user,locations,avgs,all_sims)
+    return (x)
+    #print (x)
+    #print("--- %s seconds ---" % (time.time() - start_time))
